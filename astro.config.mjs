a03652364b5d928f398939f1ff52834a3c3a3b54
Carlos Aranda Calatayud <caracal188@g.educaand.es>
2026-05-03 00:40:45 +0200

    Añadir configuración de Vercel y archivo de reescrituras para el enrutamiento

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-// astro.config.mjs
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless"; // O 'serverless'
 
 export default defineConfig({
   output: "server",
-  adapter: vercel(), // Para Vercel Serverless
+  adapter: vercel({
+    // A veces esto es necesario
+    webAnalytics: { enabled: false },
+  }),
 });
