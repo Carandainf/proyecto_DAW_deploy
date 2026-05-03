ab6aebaf26c35a4cf8106ad1e588fca150e6cf55
Carlos Aranda Calatayud <caracal188@g.educaand.es>
2026-05-03 20:10:52 +0200

    Eliminar endpoint GET de prueba en la API

--- src/pages/api/test.ts
+++
@@ -1,6 +0,0 @@
-export const GET = () => {
-  return new Response(JSON.stringify({ mensaje: "¡Hola! La API funciona." }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
-};
