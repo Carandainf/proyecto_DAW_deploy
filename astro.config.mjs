f99a2303ddf2a8980c5f7b61683f1d3cc1b85d96
Carlos Aranda Calatayud <caracal188@g.educaand.es>
2026-05-10 19:15:03 +0200

    Corrigiendo Host para  Render

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node"; // <--- Cambio vercel por node
+import node from "@astrojs/node";
 
 export default defineConfig({
   output: "server",
   adapter: node({
-    mode: "standalone", // <--- Esto es necesario para Render
+    mode: "standalone",
   }),
+  // ESTE BLOQUE ES PARA QUE EL SERVIDOR RENDER ESCUCHE EN TODAS LAS INTERFACES DE RED Y NO SOLO EN LOCALHOST
+  server: {
+    host: true, // Esto hace que escuche en 0.0.0.0 en lugar de localhost
+    port: 10000,
+  },
   security: {
-    checkOrigin: false, // Lo mantengo por seguridad en los formularios
+    checkOrigin: false,
   },
 });
